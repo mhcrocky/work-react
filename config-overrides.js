9a2a2fc382c3c9d924b5ac5b25823e826068e68a
aktwork <aktwork7@gmail.com>
2021-07-26 09:12:34 -0700

    add react rewireds

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,14 @@ const rewireAliases = require('react-app-rewire-aliases')
 
 module.exports = function override(config, env) {
   config = rewireAliases.aliasesOptions({
-    '@src': path.resolve(__dirname,   'src'),
-    '@fire': path.resolve(__dirname,  'src/firebase'),
-    '@page': path.resolve(__dirname,  'src/page'),
+    '@src'  : path.resolve(__dirname, 'src'),
+    '@fire' : path.resolve(__dirname, 'src/firebase'),
+    '@page' : path.resolve(__dirname, 'src/page'),
     '@redux': path.resolve(__dirname, 'src/redux'),
-    '@util': path.resolve(__dirname,  'src/util'),
-    '@core': path.resolve(__dirname,  'src/core'),
-    '@img': path.resolve(__dirname,   'src/asset/img'),
-    '@scss': path.resolve(__dirname,  'src/asset/scss'),
+    '@util' : path.resolve(__dirname, 'src/util'),
+    '@core' : path.resolve(__dirname, 'src/core'),
+    '@img'  : path.resolve(__dirname, 'src/asset/img'),
+    '@scss' : path.resolve(__dirname, 'src/asset/scss'),
   })(config, env)
 
   return config
